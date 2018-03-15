e0907946299a7cd7fbc46fe5c4b8050c567aa978
BogdanDjo <djob181410@mvstudent.org>
2018-03-15 10:09:04 -0500

    Strategy and Team name

--- team1.py
+++ team1.py
@@ -6,8 +6,8 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
+team_name = 'Bogdan and Sarah' # Only 10 chars displayed.
+strategy_name = 'test'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
@@ -68,4 +68,4 @@ if __name__ == '__main__':
               result='b')     
               
                       
-  team_name='Bogdan and Sarah'        
+       
